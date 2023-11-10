74affacedd5cc86ecf8b4336acb850fd7e3de694
Matheus Oliveira <matheushso99@hotmail.com>
2023-11-09 22:19:25 -0300

    adicionar get e set

--- src/main/java/br/com/coop/voting/backend/domain/model/Associado.java
+++ src/main/java/br/com/coop/voting/backend/domain/model/Associado.java
@@ -1,13 +1,32 @@
 package br.com.coop.voting.backend.domain.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Associado {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long cpf;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
 }
