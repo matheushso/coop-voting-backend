fae886fea4ddcd9a5650c37afcfc4279d0f80346
Matheus Oliveira <matheushso99@hotmail.com>
2023-11-09 21:11:49 -0300

    initial commit

---
+++ src/main/java/br/com/coop/voting/backend/CoopVotingBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.coop.voting.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopVotingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoopVotingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/coop/voting/backend/CoopVotingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coop.voting.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopVotingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
