78f32353e7c4b36538ecd80bfcc213207b405085
Matheus Oliveira <matheushso99@hotmail.com>
2023-11-11 18:33:03 -0300

    criar tabela sessao votção

---
+++ src/main/resources/db/migration/V003__criar_tabela_sessaovotacao.sql
@@ -0,0 +1,14 @@
+create table sessao_votacao
+(
+    id              bigint not null auto_increment,
+    pauta_id        bigint not null,
+    tempo_abertura  time   not null,
+    data_inicio     datetime(6) not null,
+    data_fechamento datetime(6) not null,
+
+    primary key (id)
+) engine=InnoDB default charset=utf8mb4;
+
+alter table sessao_votacao
+    add constraint fk_sessao_votacao_pauta
+        foreign key (pauta_id) references pauta (id);
