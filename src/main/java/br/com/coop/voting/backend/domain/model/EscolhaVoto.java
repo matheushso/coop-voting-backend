d72e304ca44ff6cc039f12ecf4ca3f09c47756ce
Matheus Oliveira <matheushso99@hotmail.com>
2023-11-11 18:30:58 -0300

    verificar se voto informado é válido

--- src/main/java/br/com/coop/voting/backend/domain/model/EscolhaVoto.java
+++ src/main/java/br/com/coop/voting/backend/domain/model/EscolhaVoto.java
@@ -1,9 +1,13 @@
 package br.com.coop.voting.backend.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.text.Normalizer;
+
 public enum EscolhaVoto {
 
     SIM("Sim"),
-    NAO("Não");
+    NAO("Nao");
 
     private String descricao;
 
@@ -12,6 +16,30 @@ public enum EscolhaVoto {
     }
 
     public String getDescricao() {
+        return descricao;
+    }
+
+    public static String retornarVotoValido(String voto) {
+        if (voto == null) {
+            throw new IllegalArgumentException("Não informado voto.");
+        }
+        voto = removerAcentos(voto);
+
+        for (EscolhaVoto escolha : EscolhaVoto.values()) {
+            if (escolha.getDescricao().equalsIgnoreCase(voto)) {
+                return escolha.toString();
+            }
+        }
+
+        throw new IllegalArgumentException(String.format("Valor inválido informado para escolha de voto. Valor informado %s.", voto));
+    }
+
+    private static String removerAcentos(String voto) {
+        return Normalizer.normalize(voto, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+    }
+
+    @JsonValue
+    public String toValue() {
         return descricao;
     }
 }
