e9252167a1f04851ab69fe40b0a9dc0050be2caf
Matheus Oliveira <matheushso99@hotmail.com>
2023-11-12 10:10:04 -0300

    adicionar campo para verificar se resultado da sessao foi publicado

--- src/main/resources/db/migration/V003__criar_tabela_sessaovotacao.sql
+++ src/main/resources/db/migration/V003__criar_tabela_sessaovotacao.sql
@@ -1,10 +1,11 @@
 create table sessao_votacao
 (
-    id              bigint not null auto_increment,
-    pauta_id        bigint not null,
-    tempo_abertura  time   not null,
-    data_inicio     datetime(6) not null,
-    data_fechamento datetime(6) not null,
+    id                 bigint not null auto_increment,
+    pauta_id           bigint not null,
+    tempo_abertura     time   not null,
+    data_inicio        datetime(6) not null,
+    data_fechamento    datetime(6) not null,
+    resultado_publicado tinyint(1) not null,
 
     primary key (id)
 ) engine=InnoDB default charset=utf8mb4;
